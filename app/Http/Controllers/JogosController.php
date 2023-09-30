123a0f1140700f9fb5c5eb6fe1febf44e2a3a09e
Felipe Kurjata <fkurjata@gmail.com>
2023-09-30 13:23:24 -0300

    adição da tabela com bootstrap

--- app/Http/Controllers/JogosController.php
+++ app/Http/Controllers/JogosController.php
@@ -2,14 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Jogo;
 use Illuminate\Http\Request;
 
 class JogosController extends Controller
 {
     public function index()
     {
-        $nome = 'GTA';
-        $id = 1;
-        return view('jogos.index', ['nome'=>$nome, 'id'=>$id]);
+        $jogos = Jogo::all();
+        //dd($jogos);
+        return view('jogos.index', ['jogos'=>$jogos]);
     }
 }
--- resources/views/jogos/index.blade.php
+++ resources/views/jogos/index.blade.php
@@ -3,6 +3,29 @@
 @section('title', 'Games')
 
 @section('content')
-    <h1>teste</h1>
+    <h1>Lista de Jogos</h1>
+
+    <table class="table">
+        <thead class="table-light">
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nome</th>
+                <th scope="col">Categoria</th>
+                <th scope="col">Ano de Criação</th>
+                <th scope="col">Valor</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($jogos as $jogo)
+            <tr>
+                <th>{{ $jogo->id }}</th>
+                <th>{{ $jogo->nome }}</th>
+                <th>{{ $jogo->categoria }}</th>
+                <th>{{ $jogo->ano_criacao }}</th>
+                <th>R$ {{ $jogo->valor }}</th>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
 @endsection
